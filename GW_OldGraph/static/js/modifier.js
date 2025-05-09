e48d446245ce6b19162a2a55c4f8c78abe462359
JerryGuo2001 <jianleg@uci.edu>
2025-05-09 10:22:08 -0700

    update debug off

--- GW_OldGraph/static/js/modifier.js
+++ GW_OldGraph/static/js/modifier.js
@@ -1,5 +1,5 @@
 //debug moode on/off
-debugmode= true
+debugmode= false
 if (debugmode==true){
   n_learning_trial=1 //This determine the number of learning trial you want in total
   n_direct_trial=1 //how many direct trial you want
